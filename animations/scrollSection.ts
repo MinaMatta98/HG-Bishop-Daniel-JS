937d1295e11fdf03bbdc69dd8df76702a4d6461c
Mina Matta <minamatta98@gmail.com>
2024-08-21 11:45:31 +1000

    Slight code refactor

--- animations/scrollSection.ts
+++ animations/scrollSection.ts
@@ -4,24 +4,53 @@ import { References } from './references';
 
 export class ScrollSection {
   public static initAnimation = () => {
-    const blocks: JQuery<HTMLElement> = $(References.homePageClasses.slideBlockClass);
     const wrapper: JQuery<HTMLElement> = $(References.homePageClasses.scheduleWrapper);
-    console.log('scroll');
+    const blocks: JQuery<HTMLElement> = $(References.homePageClasses.slideBlockClass);
 
     blocks.each((index, element) => {
+      console.log((index + 1) * 80 + 'vh');
       switch (index) {
         case 0:
           gsap.from(element, {
             scrollTrigger: {
-              trigger: wrapper,
+              trigger: element,
               start: 'top top',
+              end: 'bottom +=50%',
               markers: true,
+              scrub: true,
             },
+            height: '80vh',
+            ease: 'linear',
           });
         case 1:
-
-        case 2:
+          gsap.from(element, {
+            scrollTrigger: {
+              trigger: element,
+              start: 'top 80%',
+              end: 'top 45%',
+              markers: true,
+              scrub: true,
+            },
+            height: '80vh',
+            ease: 'linear',
+          });
+        default:
+          gsap.set(element, { height: '80vh' });
       }
+      //     switch (index) {
+      //       case 0:
+      //});
+      //case 1:
+      //  gsap.from(element, {
+      //    scrollTrigger: {
+      //      trigger: wrapper,
+      //      start: 'top top',
+      //      markers: true,
+      //    },
+      //  });
+      //
+      //case 2:
+      //}
     });
   };
 }
