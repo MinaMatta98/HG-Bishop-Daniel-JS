5efe0442bfd8d0e2ad1f20be563748481cc13eea
Mina Matta <minamatta98@gmail.com>
2024-08-22 09:51:28 +1000

    Added scroll animations for home page schedule sections

--- animations/scrollSection.ts
+++ animations/scrollSection.ts
@@ -1,56 +1,23 @@
-import { gsap, ScrollTrigger } from 'gsap/all';
+import { gsap } from 'gsap/all';
 
 import { References } from './references';
 
 export class ScrollSection {
   public static initAnimation = () => {
-    const wrapper: JQuery<HTMLElement> = $(References.homePageClasses.scheduleWrapper);
+    //const wrapper: JQuery<HTMLElement> = $(References.homePageClasses.scheduleWrapper);
     const blocks: JQuery<HTMLElement> = $(References.homePageClasses.slideBlockClass);
 
-    blocks.each((index, element) => {
-      console.log((index + 1) * 80 + 'vh');
-      switch (index) {
-        case 0:
-          gsap.from(element, {
-            scrollTrigger: {
-              trigger: element,
-              start: 'top top',
-              end: 'bottom +=50%',
-              markers: true,
-              scrub: true,
-            },
-            height: '80vh',
-            ease: 'linear',
-          });
-        case 1:
-          gsap.from(element, {
-            scrollTrigger: {
-              trigger: element,
-              start: 'top 80%',
-              end: 'top 45%',
-              markers: true,
-              scrub: true,
-            },
-            height: '80vh',
-            ease: 'linear',
-          });
-        default:
-          gsap.set(element, { height: '80vh' });
-      }
-      //     switch (index) {
-      //       case 0:
-      //});
-      //case 1:
-      //  gsap.from(element, {
-      //    scrollTrigger: {
-      //      trigger: wrapper,
-      //      start: 'top top',
-      //      markers: true,
-      //    },
-      //  });
-      //
-      //case 2:
-      //}
+    $(blocks).each((_, block) => {
+      gsap.from(block, {
+        scrollTrigger: {
+          trigger: block,
+          start: 'top 60%',
+          end: 'top 30%',
+          scrub: 1,
+        },
+        translateY: 100,
+        opacity: 0,
+      });
     });
   };
 }
