cbefe1547fbe65e5f1bdd2ba34a82a60a388ecaf
Mina Matta <minamatta98@gmail.com>
2024-08-22 10:01:29 +1000

    Refactor

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
-import { barbaInit } from './barba';
 import $ from 'jquery';
 
+import { barbaInit } from './barba';
+
 for (let index = 0; index < 10; index++) {
-	console.log(index)
+  console.log(index);
 }
 
 const containerSelector = '.main-wrapper';
@@ -10,15 +11,15 @@ const containerSelector = '.main-wrapper';
 export const DOMAIN = 'https://hg-bishop-daniels-website.webflow.io/';
 
 const observer = new MutationObserver((mutationsList, observer) => {
-    for (let mutation of mutationsList) {
-        if (mutation.type === 'childList') {
-            if ($(containerSelector).length) {
-                barbaInit();
-                observer.disconnect();
-                break;
-            }
-        }
+  for (const mutation of mutationsList) {
+    if (mutation.type === 'childList') {
+      if ($(containerSelector).length) {
+        barbaInit();
+        observer.disconnect();
+        break;
+      }
     }
+  }
 });
 
 observer.observe($(document.documentElement)[0], { childList: true, subtree: true });
