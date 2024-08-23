40e2fa1355be3f94ef13027049f92ce9763751d1
Mina Matta <minamatta98@gmail.com>
2024-08-23 12:59:10 +1000

    Major refactor, moving functionality to homepage class

--- animations/animations.ts
+++ animations/animations.ts
@@ -1,40 +1,29 @@
 import type { ISchemaPage } from '@barba/core/dist/core/src/src/defs';
-import { gsap, ScrollTrigger } from 'gsap/all';
+import { gsap } from 'gsap/all';
 import $ from 'jquery';
 
 import { BioAnimations } from './bio-animations';
-import { CarouselAnimations } from './carousel';
 import { CursorAnimations } from './cursor-animations';
 import { FooterAnimations } from './footerAnimations';
-import { GlobeAnimation } from './globe';
-import { LogoAnimations } from './logo-animations';
+import { HomePageAnimations } from './homepage-animations';
 import { NavBarAnimations } from './navbar-animations';
 import { References } from './references';
-import { ScrollSection } from './scrollSection';
 import { TOCAnimations } from './toc';
 
 export class Animations {
   private static _cursorAnimator = CursorAnimations;
   private static _navBarAnimator: NavBarAnimations = new NavBarAnimations();
-  private static _carousel = new CarouselAnimations();
-  private static _scrollAnimator = ScrollSection;
   private static _bioAnimator = BioAnimations;
   private static _footerAnimator = FooterAnimations;
   private static _tocAnimator = new TOCAnimations();
-  private static _globeAnimation = new GlobeAnimation();
-  private static _logoAnimator = LogoAnimations;
-
-  public static initGlobe = () => {
-    this._globeAnimation.init();
-    this._globeAnimation.animateGlobeBlock();
-  };
+  private static _homePageAnimator = HomePageAnimations;
 
   public static disposeGlobe = () => {
-    this._globeAnimation.dispose();
+    this._homePageAnimator.disposeGlobe();
   };
 
-  public static initScrollSection = () => {
-    this._scrollAnimator.initAnimation();
+  public static initHomePage = async (initTime: number, isFirstLoad: boolean) => {
+    await this._homePageAnimator.animateHomePage(initTime, isFirstLoad);
   };
 
   public static cursorWhite = (): void => {
@@ -54,10 +43,6 @@ export class Animations {
 
   public static setOpaque = (identifier: gsap.TweenTarget): void => {
     gsap.set(identifier, { opacity: '1' });
-  };
-
-  public static logoAnimation = () => {
-    this._logoAnimator.logoAnimation();
   };
 
   public static handleMinistrySlider = (): void => {
@@ -105,7 +90,7 @@ export class Animations {
     view: boolean,
     display?: gsap.TweenValue
   ) {
-    gsap.to(identifier, { display: view ? display! : 'none' });
+    gsap.set(identifier, { display: view ? display! : 'none' });
   }
 
   public static handleTransitionAnimation = async (inView: boolean): Promise<void> => {
@@ -125,49 +110,16 @@ export class Animations {
     if (!inView) this.displayShow(References.transitionClasses.transitionClass, false);
   };
 
-  public static VideoAnimation = async (): Promise<void> => {
-    gsap.set(References.logoClasses.topLogoClass, { translateY: '-15em' });
-    gsap.set(References.logoClasses.centerLogoClass, { opacity: 0, translateY: 150 });
-    gsap.set(References.homePageClasses.heroHeadingClass, { opacity: 0, translateY: 150 });
-
-    gsap.to(References.logoClasses.centerLogoClass, { opacity: 1, duration: 0.5 });
-    gsap.to(References.logoClasses.centerLogoClass, { translateY: 0, duration: 4 });
-    await gsap.to(References.logoClasses.topLogoClass, { translateY: '0', duration: 3 });
-
-    gsap.to(References.homePageClasses.heroHeadingClass, { opacity: 1, duration: 0.5 });
-    await gsap.to(References.homePageClasses.heroHeadingClass, { translateY: 0, duration: 3 });
-
-    gsap.to(References.homePageClasses.heroHeadingClass, { opacity: 0, duration: 1 });
-    await gsap.to(References.logoClasses.centerLogoClass, { opacity: 0, duration: 1 });
-  };
-
-  public static gsapSliderInit = () => {
-    this._carousel.initCarousel();
-    this._carousel.initializeWrappers();
-    this._carousel.initializeButtons();
-  };
-
-  public static gsapGlobeContainerExpand = () => {
-    const container = $(References.homePageClasses.globeContainerClass);
-
-    gsap.from(container, {
-      scrollTrigger: {
-        trigger: container,
-        start: 'top 70%',
-        end: 'top top',
-        scrub: 1,
-      },
-      maxWidth: 1640,
-      borderRadius: '3rem',
-    });
-  };
-
   public static animateBioHeading = () => {
     this._bioAnimator.animateHeading();
   };
 
   public static animateBioTimeline = () => {
     this._bioAnimator.animateTimeline();
+  };
+
+  public static animateBioLogo = () => {
+    this._bioAnimator.animateBioLogo();
   };
 
   public static footerAnimateBlue = () => {
@@ -178,11 +130,7 @@ export class Animations {
     this._footerAnimator.animateFooterWhite();
   };
 
-  public static animateBioLogo = () => {
-    this._bioAnimator.animateBioLogo();
-  };
-
-  public static animateToc = () => {
-    this._tocAnimator.tocAnimation();
+  public static animateToc =async  () => {
+    await this._tocAnimator.tocAnimation();
   };
 }
