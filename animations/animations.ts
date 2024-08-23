6a8202022292c93f19b5b4d175d30322f218a04d
Mina Matta <minamatta98@gmail.com>
2024-08-23 11:15:50 +1000

    Added toc animation handle and logo animation refactor

--- animations/animations.ts
+++ animations/animations.ts
@@ -1,5 +1,5 @@
 import type { ISchemaPage } from '@barba/core/dist/core/src/src/defs';
-import { Flip, gsap, ScrollTrigger } from 'gsap/all';
+import { gsap, ScrollTrigger } from 'gsap/all';
 import $ from 'jquery';
 
 import { BioAnimations } from './bio-animations';
@@ -7,15 +7,11 @@ import { CarouselAnimations } from './carousel';
 import { CursorAnimations } from './cursor-animations';
 import { FooterAnimations } from './footerAnimations';
 import { GlobeAnimation } from './globe';
+import { LogoAnimations } from './logo-animations';
 import { NavBarAnimations } from './navbar-animations';
 import { References } from './references';
 import { ScrollSection } from './scrollSection';
-
-interface LogoTransition {
-  scrollSection: JQuery<HTMLElement>;
-  targetContainer: JQuery<HTMLElement>;
-  removeClass: boolean;
-}
+import { TOCAnimations } from './toc';
 
 export class Animations {
   private static _cursorAnimator = CursorAnimations;
@@ -24,6 +20,9 @@ export class Animations {
   private static _scrollAnimator = ScrollSection;
   private static _bioAnimator = BioAnimations;
   private static _footerAnimator = FooterAnimations;
+  private static _tocAnimator = new TOCAnimations();
+  private static _globeAnimation = new GlobeAnimation();
+  private static _logoAnimator = LogoAnimations;
 
   public static initGlobe = () => {
     this._globeAnimation.init();
@@ -58,103 +57,7 @@ export class Animations {
   };
 
   public static logoAnimation = () => {
-    const logo = $('.ths07-brand');
-    const backgroundVideo = $('.background-video');
-    const standardBottom = 'bottom bottom-=20px';
-
-    const sections = {
-      sectionHero: $('.section-hero-home'),
-      newsSection: $('.news-section'),
-      sermonsSection: $('.sermons-section'),
-      feedbackSection: $('.feedback-section'),
-      churchesSection: $('.spline-section'),
-      timelineSection: $('.timeline-section'),
-    };
-
-    const containers = {
-      logoContainer: $('.logo-embed'),
-      //newslogoContainer: $('.news-logo-embed'),
-      //sermonslogoContainer: $('.sermons-logo-embed'),
-      //feedbacklogoContainer: $('.feedback-logo-embed'),
-      logoVideoContainer: $('.logo'),
-      //churcheslogoContainer: $('.churches-logo-embed'),
-      //timelinelogoContainer: $('.timeline-logo-embed'),
-    };
-
-    const moveLogo = (removeClass: boolean, targetContainer: JQuery<HTMLElement>) => {
-      const currentState = Flip.getState(logo);
-      if (removeClass) {
-        logo.removeClass('morphed');
-      }
-      if (!logo.hasClass('morphed') && !removeClass) logo.addClass('morphed');
-      targetContainer[0].appendChild(logo[0]);
-      Flip.from(currentState, { duration: 1, ease: 'power3.inOut', scale: true });
-    };
-
-    const videoTransition: LogoTransition = {
-      scrollSection: backgroundVideo,
-      targetContainer: containers.logoVideoContainer,
-      removeClass: true,
-    };
-
-    const bioTransition: LogoTransition = {
-      scrollSection: sections.sectionHero,
-      targetContainer: containers.logoContainer,
-      removeClass: false,
-    };
-
-    //const newsTransition: LogoTransition = {
-    //  scrollSection: sections.newsSection,
-    //  targetContainer: containers.newslogoContainer,
-    //  removeClass: false,
-    //};
-    //
-    //const sermonsTransition: LogoTransition = {
-    //  scrollSection: sections.sermonsSection,
-    //  targetContainer: containers.sermonslogoContainer,
-    //  removeClass: false,
-    //};
-    //
-    //const feedbackTransition: LogoTransition = {
-    //  scrollSection: sections.feedbackSection,
-    //  targetContainer: containers.feedbacklogoContainer,
-    //  removeClass: false,
-    //};
-    //
-    //const churchesTransition: LogoTransition = {
-    //  scrollSection: sections.churchesSection,
-    //  targetContainer: containers.churcheslogoContainer,
-    //  removeClass: false,
-    //};
-    //
-    //const timelineTransition: LogoTransition = {
-    //  scrollSection: sections.timelineSection,
-    //  targetContainer: containers.timelinelogoContainer,
-    //  removeClass: false,
-    //};
-
-    [
-      videoTransition,
-      bioTransition,
-      //newsTransition,
-      //sermonsTransition,
-      //feedbackTransition,
-      //churchesTransition,
-      //timelineTransition,
-    ].forEach(({ scrollSection, targetContainer, removeClass }) => {
-      ScrollTrigger.create({
-        trigger: scrollSection,
-        start: 'top 60%',
-        end: standardBottom,
-        scrub: 1,
-        onEnterBack() {
-          moveLogo(removeClass, targetContainer);
-        },
-        onEnter() {
-          moveLogo(removeClass, targetContainer);
-        },
-      });
-    });
+    this._logoAnimator.logoAnimation();
   };
 
   public static handleMinistrySlider = (): void => {
@@ -293,5 +196,9 @@ export class Animations {
 
   public static animateBioLogo = () => {
     this._bioAnimator.animateBioLogo();
+  };
+
+  public static animateToc = () => {
+    this._tocAnimator.tocAnimation();
   };
 }
