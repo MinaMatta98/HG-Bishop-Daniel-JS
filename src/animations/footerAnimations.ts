f66d7bcc79082e241ea9c6f1f6259b226622b6b2
Mina Matta <minamatta98@gmail.com>
2024-10-07 16:00:15 +1100

    Moved all footer animations into their own file

--- src/animations/footerAnimations.ts
+++ src/animations/footerAnimations.ts
@@ -1,33 +1,58 @@
+import { Animations } from './animations';
 import { References } from './references';
 
 export class FooterAnimations {
   public static animateFooterBlue = (): void => {
     $(() => {
-      const footerSection = document.querySelector(References.footerClasses.footerSectionClass);
-      const footerCta = document.querySelector(References.footerClasses.footerCtaClass);
-      const footerBottom = document.querySelector(References.footerClasses.footerBottomClass);
-      const footerSocial = document.querySelector(References.footerClasses.footerSocialClass);
+      const footerSection = $(References.footerClasses.footerSectionClass);
+      const footerCta = $(References.footerClasses.footerCtaClass);
+      const footerBottom = $(References.footerClasses.footerBottomClass);
+      const footerSocial = $(References.footerClasses.footerSocialClass);
 
-      $(footerSection).css('background-color', 'var(--cursor-inner)');
-      $(footerCta).css('background-color', 'white');
-      $(footerCta).find('div').css('color', 'var(--cursor-inner)');
-      $(footerBottom).css('color', 'white');
-      $(footerSocial).find('a').css('color', 'white');
+      footerSection.css('background-color', 'var(--cursor-inner)');
+      footerCta.css('background-color', 'white');
+      footerCta.find('div').css('color', 'var(--cursor-inner)');
+      footerBottom.css('color', 'white');
+      footerSocial.find('a').css('color', 'white');
+
+      footerSection.on('mouseenter', () => {
+        Animations.cursorWhite();
+      });
+
+      footerCta.on('mouseenter', () => {
+        Animations.cursorBlue();
+      });
+
+      footerCta.on('mouseleave', () => {
+        Animations.cursorWhite();
+      });
     });
   };
 
   public static animateFooterWhite = () => {
     $(() => {
-      const footerSection = document.querySelector(References.footerClasses.footerSectionClass);
-      const footerCta = document.querySelector(References.footerClasses.footerCtaClass);
-      const footerBottom = document.querySelector(References.footerClasses.footerBottomClass);
-      const footerSocial = document.querySelector(References.footerClasses.footerSocialClass);
+      const footerSection = $(References.footerClasses.footerSectionClass);
+      const footerCta = $(References.footerClasses.footerCtaClass);
+      const footerBottom = $(References.footerClasses.footerBottomClass);
+      const footerSocial = $(References.footerClasses.footerSocialClass);
 
-      $(footerSection).css('background-color', 'white');
-      $(footerCta).css('background-color', 'var(--cursor-inner)');
-      $(footerCta).find('div').css('color', 'white');
-      $(footerBottom).css('color', 'black');
-      $(footerSocial).find('a').css('color', 'var(--cursor-inner)');
+      footerSection.css('background-color', 'white');
+      footerCta.css('background-color', 'var(--cursor-inner)');
+      footerCta.find('div').css('color', 'white');
+      footerBottom.css('color', 'black');
+      footerSocial.find('a').css('color', 'var(--cursor-inner)');
+
+      footerSection.on('mouseenter', () => {
+        Animations.cursorBlue();
+      });
+
+      footerCta.on('mouseenter', () => {
+        Animations.cursorWhite();
+      });
+
+      footerCta.on('mouseleave', () => {
+        Animations.cursorBlue();
+      });
     });
   };
 }
