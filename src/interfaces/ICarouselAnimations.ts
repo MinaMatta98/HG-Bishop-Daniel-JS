38674ce8f9cdb6c3e54f3c71df81c365521bba14
Mina Matta <minamatta98@gmail.com>
2024-11-26 21:35:45 +1100

    ICarouselAnimations is more abstract

--- src/interfaces/ICarouselAnimations.ts
+++ src/interfaces/ICarouselAnimations.ts
@@ -1,9 +1,17 @@
 import type { IGsapPageAnimations } from './IGsapPageAnimations';
 import type { IMouseEventAnimations } from './IMouseEventAnimations';
 
-interface ICarouselAnimations extends IGsapPageAnimations, IMouseEventAnimations {
+export interface ICarouselAnimations extends IGsapPageAnimations, IMouseEventAnimations {
+  pageElemets: Map<string, JQuery<HTMLElement>>;
+  initElements: () => void;
   animateCarousel: () => void;
-  nextSlide?: () => void;
-  prevSlide?: () => void;
+  nextSlide?: (i: number, el: JQuery<HTMLElement>) => void;
+  prevSlide?: (i: number, el: JQuery<HTMLElement>) => void;
   nthSlide?: (n: number) => void;
+  animatePins?: (i: number, el: JQuery<HTMLElement>) => void;
+  animateFocusedControls?: (el: JQuery<HTMLElement>) => void;
+  animateUnfocusedControls?: (el: JQuery<HTMLElement>) => void;
+  animateFocusedSlide?: (el: JQuery<HTMLElement>) => void;
+  animateUnfocusedSlide?: (el: JQuery<HTMLElement>) => void;
+  animateButtons: () => void;
 }
